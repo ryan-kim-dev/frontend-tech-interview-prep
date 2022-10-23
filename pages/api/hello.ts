251bfeca255308fbcd3996056dab6d287d0e6153
ryan-kim-dev <ryankim.h.dev@gmail.com>
2022-10-23 23:10:20 +0900

    fix: eslint 설정값 변경

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 import { NextApiRequest, NextApiResponse } from 'next';
-import FirebaseAdmin from '../../models/firebase_admin';
+import FirebaseAdmin from '@/models/firebase_admin';
 
 export default function handler(_: NextApiRequest, res: NextApiResponse) {
   // firebase의 'test'콜렉션에 접근
